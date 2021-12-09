7f08842effeb311e70bfa85d0c57b514dd140a87
Kusdianto20 <Kusdiantoputra17@gmail.com>
2021-12-09 15:08:50 +0700

    first commit

---
+++ app/src/main/java/com/bangkitui/tessamapta2/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.bangkitui.tessamapta2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
